86709f3339300056636bd8997a4edb55233bd0d9
ldydek <91955435+ldydek@users.noreply.github.com>
2023-10-05 08:58:28 +0200

    Delete Lab 1 directory

--- Lab 1/active-record/src/main/java/pl/edu/agh/iisg/to/Main.java
+++
@@ -1,8 +0,0 @@
-package pl.edu.agh.iisg.to;
-
-public class Main {
-
-    public static void main(String[] args) {
-        //This time we execute only junit tests
-    }
-}
--- Lab 1/orm/src/main/java/pl/edu/agh/iisg/to/Main.java
+++
@@ -1,9 +0,0 @@
-package pl.edu.agh.iisg.to;
-
-public class Main {
-
-    public static void main(String[] args) {
-
-    }
-
-}
