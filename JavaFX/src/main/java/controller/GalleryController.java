a5b6190bb348138fba77877baa7535b9c787eb40
ldydek <lukaszdydek72@gmail.com>
2023-10-26 09:18:29 +0200

    model controller task done

--- JavaFX/src/main/java/controller/GalleryController.java
+++ JavaFX/src/main/java/controller/GalleryController.java
@@ -1,7 +1,10 @@
 package controller;
 
 import javafx.fxml.FXML;
-
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import model.Gallery;
 import model.Photo;
 
@@ -10,17 +13,42 @@ public class GalleryController {
     private Gallery galleryModel;
 
     @FXML
+    private TextField imageNameField;
+
+    @FXML
+    private ImageView imageView;
+
+    @FXML
+    private ListView<Photo> imagesListView;
+
+    @FXML
     public void initialize() {
-        // TODO additional FX controls initialization
+        imagesListView.setCellFactory(param -> new ListCell<>() {
+            @Override
+            protected void updateItem(Photo item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty) {
+                    setText(null);
+                    setGraphic(null);
+                } else {
+                    ImageView photoIcon = new ImageView(item.getPhotoData());
+                    photoIcon.setPreserveRatio(true);
+                    photoIcon.setFitHeight(50);
+                    setGraphic(photoIcon);
+                }
+            }
+        });
     }
 
     public void setModel(Gallery gallery) {
         this.galleryModel = gallery;
         bindSelectedPhoto(gallery.getPhotos().get(0));
+        this.imagesListView.setItems(gallery.getPhotos());
     }
 
     private void bindSelectedPhoto(Photo selectedPhoto) {
-        // TODO view <-> model bindings configuration
+        imageNameField.textProperty().bindBidirectional(selectedPhoto.getNameProperty());
+        imageView.imageProperty().bind(selectedPhoto.getPhotoProperty());
     }
 }
 
--- JavaFX/src/main/java/model/Photo.java
+++ JavaFX/src/main/java/model/Photo.java
@@ -1,5 +1,9 @@
 package model;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.scene.image.Image;
 
 import java.io.ByteArrayInputStream;
@@ -7,24 +11,32 @@ import java.util.UUID;
 
 public class Photo {
 
-    private String name;
+    private final StringProperty name;
 
-    private final Image photoData;
+    private final ObjectProperty<Image> photoData;
 
     public Photo(String extension, byte[] photoData) {
-        this.photoData = new Image(new ByteArrayInputStream(photoData));
-        this.name = UUID.randomUUID().toString() + "." + extension;
+        this.photoData = new SimpleObjectProperty<>(new Image(new ByteArrayInputStream(photoData)));
+        this.name = new SimpleStringProperty(UUID.randomUUID() + "." + extension);
     }
 
     public String getName() {
-        return name;
+        return this.name.getName();
     }
 
     public void setName(String name) {
-        this.name = name;
+        this.name.set(name);
     }
 
     public Image getPhotoData() {
+        return photoData.get();
+    }
+
+    public StringProperty getNameProperty() {
+        return name;
+    }
+
+    public ObjectProperty<Image> getPhotoProperty() {
         return photoData;
     }
 }
